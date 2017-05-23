51e943c0eab4f06c3045ba386384f948c0523d09
heshuai <775364468@qq.com>
2017-05-23 20:36:22 +0800

    fifth version trans string

--- Trans_param.c
+++ Trans_param.c
@@ -2,12 +2,14 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-int test=100;
+//int test=100;
 void *create (void *arg){
-	int *num;
-	num = (int *)arg;
-	printf("from main process is :%d\n",*num);
-	printf("test is :%d\n",test);
+	//int *num;
+	//num = (int *)arg;
+	//printf("from main process is :%d\n",*num);
+	//printf("test is :%d\n",test);
+	char *str =(char *)arg;
+	printf("from main process string is :%s\n",str);
 	return (void *)0;
 
 }
@@ -16,11 +18,11 @@ int main(int argc ,char *argv[])
 {
 	int ret;
 	pthread_t pd1;
-	int test=500;
-	int *attr =&test;
-	
-	ret =pthread_create(&pd1, NULL,create, (void *)attr);	
-	
+	//int test=500;
+	//int *attr =&test;
+	char *str="transfer string";
+	//ret =pthread_create(&pd1, NULL,create, (void *)attr);	
+	ret =pthread_create(&pd1, NULL,create, (void *)str);	
 	if(ret!=0){
 		printf("pthread_create error!\n");
 		exit(0);
